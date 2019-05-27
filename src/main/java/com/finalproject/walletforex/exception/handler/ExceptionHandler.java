126f985864c98ab04dc0d91887f9b5c0dfd53ee9
Agus1995 <aguswhibawa95@gmail.com>
2019-05-27 15:59:52 +0700

    Create Account Exception

--- src/main/java/com/finalproject/walletforex/exception/handler/ExceptionHandler.java
+++ src/main/java/com/finalproject/walletforex/exception/handler/ExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.finalproject.walletforex.exception.handler;
 
+import com.finalproject.walletforex.dto.CommonResponse;
+import com.finalproject.walletforex.exception.AccountNotFoundException;
 import com.finalproject.walletforex.exception.InvalidUsernameOrPasswordException;
 import com.finalproject.walletforex.exception.UserAlreadyException;
 import org.slf4j.Logger;
@@ -7,7 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import response.CommonResponse;
+
 
 @ControllerAdvice
 public class ExceptionHandler {
@@ -24,6 +26,15 @@ public class ExceptionHandler {
 
     @org.springframework.web.bind.annotation.ExceptionHandler(value = InvalidUsernameOrPasswordException.class)
     public ResponseEntity<CommonResponse> resp(InvalidUsernameOrPasswordException e){
+//        LOGGER.error(e.getMesage());
+//        LOGGER.warn(e.getMesage());
+//        LOGGER.info(e.getMesage());
+//        LOGGER.debug(e.getMesage());
+        return new ResponseEntity<CommonResponse>(new CommonResponse(e.getCode(), e.getMessage()), HttpStatus.OK);
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(value = AccountNotFoundException.class)
+    public ResponseEntity<CommonResponse> resp(AccountNotFoundException e){
 //        LOGGER.error(e.getMesage());
 //        LOGGER.warn(e.getMesage());
 //        LOGGER.info(e.getMesage());
