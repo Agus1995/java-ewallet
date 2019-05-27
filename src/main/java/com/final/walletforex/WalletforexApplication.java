3b31e8c3cc5f88c7f5ecfd729c7f764e347013cc
Agus1995 <aguswhibawa95@gmail.com>
2019-05-27 11:18:02 +0700

    Initial commit

---
+++ src/main/java/com/final/walletforex/WalletforexApplication.java
@@ -0,0 +1,13 @@
+package com.final.walletforex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletforexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletforexApplication.class, args);
+	}
+
+}
