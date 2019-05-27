10bdfc7d6fd07c1012c5a00f8becd7f672538dae
Agus1995 <aguswhibawa95@gmail.com>
2019-05-27 21:48:36 +0700

    wallet account repository

--- src/main/java/com/finalproject/walletforex/repository/WalletAccountRepository.java
+++ src/main/java/com/finalproject/walletforex/repository/WalletAccountRepository.java
@@ -1,4 +1,13 @@
 package com.finalproject.walletforex.repository;
 
-public class WalletAccountRepository {
+import com.finalproject.walletforex.model.WalletAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface WalletAccountRepository extends JpaRepository<WalletAccount, Integer> {
+    List<WalletAccount> findByAccountCustomer_Cif(String cif);
+
 }
