d3d6942478e381b4fe0dded6b0902b8d1042b65b
Agus1995 <aguswhibawa95@gmail.com>
2019-05-28 22:40:28 +0700

    Update beans

--- src/main/java/com/finalproject/walletforex/config/beans/WalletAccountBeans.java
+++ src/main/java/com/finalproject/walletforex/config/beans/WalletAccountBeans.java
@@ -2,10 +2,12 @@ package com.finalproject.walletforex.config.beans;
 
 import com.finalproject.walletforex.dao.WalletAccountDao;
 import com.finalproject.walletforex.dao.implement.WalletAccountDaoImpl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class WalletAccountBeans {
+    @Bean
     public WalletAccountDao walletAccountDao(){
         return new WalletAccountDaoImpl();
     }
--- src/main/java/com/finalproject/walletforex/config/beans/WalletBeans.java
+++ src/main/java/com/finalproject/walletforex/config/beans/WalletBeans.java
@@ -2,10 +2,12 @@ package com.finalproject.walletforex.config.beans;
 
 import com.finalproject.walletforex.dao.WalletDao;
 import com.finalproject.walletforex.dao.implement.WalletDaoImpl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class WalletBeans {
+    @Bean
     public WalletDao walletDao(){
         return new WalletDaoImpl();
     }
