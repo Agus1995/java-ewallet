2996258fed87b5a4508e86d04df3fb62809ac372
Agus1995 <aguswhibawa95@gmail.com>
2019-05-29 16:24:53 +0700

    Add buy

--- src/main/java/com/finalproject/walletforex/dao/KursDao.java
+++ src/main/java/com/finalproject/walletforex/dao/KursDao.java
@@ -5,6 +5,9 @@ import com.finalproject.walletforex.model.Kurs;
 public interface KursDao {
     double sell(String code, double val);
     double buy(String code, double val);
+    double buyMoney(String code, double val);
+    double getBuyPrice(String code);
+    double getSellPrice(String code);
     Kurs getByCode(String code);
     Kurs updateKurs(Kurs kurs);
 }
