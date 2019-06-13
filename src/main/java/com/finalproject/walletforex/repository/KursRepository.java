305bd676a16d4bb0d078756ba5d1961a0f5381f1
Agus1995 <aguswhibawa95@gmail.com>
2019-06-13 20:26:33 +0700

    Kurs Repository update

--- src/main/java/com/finalproject/walletforex/repository/KursRepository.java
+++ src/main/java/com/finalproject/walletforex/repository/KursRepository.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface KursRepository extends JpaRepository<Kurs, Integer> {
+    Kurs findByCcy1AndCcy2(String ccy1, String ccy2);
     Kurs findByCcy2(String ccy);
     @Query("SELECT k FROM Kurs k WHERE k.date = (SELECT MAX(ku.date) FROM Kurs ku) AND k.ccy1= :curency1 AND k.ccy2= :curency2")
     Kurs findNewestKurs(
