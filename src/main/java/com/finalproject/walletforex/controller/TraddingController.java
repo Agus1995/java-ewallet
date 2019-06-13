751f6677721dbbac666679a4b6c24345b6fc94a3
Agus1995 <aguswhibawa95@gmail.com>
2019-06-14 06:11:52 +0700

    Add tradding buy and sell

--- src/main/java/com/finalproject/walletforex/controller/TraddingController.java
+++ src/main/java/com/finalproject/walletforex/controller/TraddingController.java
@@ -1,5 +1,45 @@
 package com.finalproject.walletforex.controller;
 
-public class TraddingController {
+import com.finalproject.walletforex.dao.TraddingDao;
+import com.finalproject.walletforex.dto.CommonResponse;
+import com.finalproject.walletforex.dto.TraddingDto;
+import com.finalproject.walletforex.exception.AccountNotFoundException;
+import com.finalproject.walletforex.exception.BalanceNotEnoughException;
+import com.finalproject.walletforex.model.ForexTradding;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class TraddingController {
+    private static final String SELL = "/sell"; //path login
+    private static final String BUY = "/buy"; //path login
+
+    @Autowired
+    private TraddingDao traddingDao;
+
+//    @GetMapping(path = SELL)
+//    public CommonResponse<ForexTradding> profile(@PathVariable(value = "cif") String cif) throws WalletNotFoundException {
+//        CommonResponse<ForexTradding> response = new CommonResponse<>();
+//        ForexTradding forexTradding = traddingDao.check(cif);
+//        response.setData(forexTradding);
+//        return response;
+//    }
+
+    @PostMapping(path = BUY)
+    public CommonResponse<ForexTradding> buy(@RequestBody TraddingDto dto) throws AccountNotFoundException, BalanceNotEnoughException {
+        CommonResponse<ForexTradding> response = new CommonResponse<>();
+        ForexTradding forexTradding = traddingDao.buy(dto);
+        response.setData(forexTradding);
+        return response;
+    }
+
+    @PostMapping(path = SELL)
+    public CommonResponse<ForexTradding> sell(@RequestBody TraddingDto dto) throws BalanceNotEnoughException {
+        CommonResponse<ForexTradding> response = new CommonResponse<>();
+        ForexTradding forexTradding = traddingDao.sell(dto);
+        response.setData(forexTradding);
+        return response;
+    }
 }
--- src/main/java/com/finalproject/walletforex/dao/TraddingDao.java
+++ src/main/java/com/finalproject/walletforex/dao/TraddingDao.java
@@ -1,11 +1,14 @@
 package com.finalproject.walletforex.dao;
 
+import com.finalproject.walletforex.dto.TraddingDto;
 import com.finalproject.walletforex.exception.AccountNotFoundException;
 import com.finalproject.walletforex.exception.BalanceNotEnoughException;
-import com.finalproject.walletforex.model.Account;
+import com.finalproject.walletforex.exception.WalletNotFoundException;
 import com.finalproject.walletforex.model.ForexTradding;
 
 public interface TraddingDao {
-    ForexTradding buy(ForexTradding forexTradding, Account account) throws AccountNotFoundException, BalanceNotEnoughException;
-    ForexTradding sell(ForexTradding forexTradding, Account account);
+    ForexTradding buy(TraddingDto traddingDto) throws AccountNotFoundException, BalanceNotEnoughException;
+    ForexTradding sell(TraddingDto traddingDto) throws BalanceNotEnoughException;
+    ForexTradding check(String cif) throws WalletNotFoundException;
+    double checksum(String cif);
 }
