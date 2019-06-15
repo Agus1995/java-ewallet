b7c932186c6c83291f26bb6c9d55be4bc93f3412
Agus1995 <aguswhibawa95@gmail.com>
2019-06-15 19:39:35 +0700

    Add Test

---
+++ src/test/java/com/finalproject/walletforex/CustomerTest.java
@@ -0,0 +1,88 @@
+package com.finalproject.walletforex;
+
+
+import com.finalproject.walletforex.dao.CustomerDao;
+import com.finalproject.walletforex.dto.CustomerDto;
+import com.finalproject.walletforex.exception.AccountNotFoundException;
+import com.finalproject.walletforex.exception.InvalidUsernameOrPasswordException;
+import com.finalproject.walletforex.exception.UserAlreadyException;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class CustomerTest {
+
+    @Autowired
+    private CustomerDao customerDao;
+
+    @Rule
+    public ExpectedException rule = ExpectedException.none();
+
+    @Before
+    public void setDao() throws UserAlreadyException {
+        for (int i = 0; i < 5; i++){
+            int row = i+1;
+            CustomerDto customer = new CustomerDto();
+            customer.setUsername(String.format("agus%s",i));
+            customer.setPassword("1234");
+            customer.setFirstName("pay");
+            customerDao.registerCustomer(customer);
+        }
+    }
+
+    @Test
+    public void getByIdCustomer() throws AccountNotFoundException {
+        assertEquals("pay", customerDao.findById("pa-01").getFirstName());
+    }
+
+    @Test
+    public void getByIdCustomerNegative() throws AccountNotFoundException {
+		rule.expect(AccountNotFoundException.class);
+        assertEquals("1", customerDao.findById("1").getCif());
+    }
+
+    @Test
+    public void loginTest() throws InvalidUsernameOrPasswordException {
+        CustomerDto customerDto = new CustomerDto();
+        customerDto.setUsername("agus1");
+        customerDto.setPassword("1234");
+        assertEquals(customerDto.getUsername(), customerDao.login(customerDto).getUsername());
+    }
+
+    @Test
+    public void negativeLoginTest() throws InvalidUsernameOrPasswordException {
+        rule.expect(InvalidUsernameOrPasswordException.class);
+        CustomerDto customerDto = new CustomerDto();
+        customerDto.setUsername("agus1");
+        customerDto.setPassword("12345");
+        assertEquals(customerDto.getUsername(), customerDao.login(customerDto).getUsername());
+    }
+
+    @Test
+    public void registerTest() throws UserAlreadyException {
+        CustomerDto customer = new CustomerDto();
+        customer.setUsername(String.format("aguswibawa"));
+        customer.setPassword("1234");
+        customer.setFirstName("pay");
+        assertEquals(customer.getUsername(), customerDao.registerCustomer(customer).getUsername());
+    }
+
+    @Test
+    public void registerNegativeTest() throws UserAlreadyException {
+        rule.expect(UserAlreadyException.class);
+        CustomerDto customer = new CustomerDto();
+        customer.setUsername(String.format("agus1"));
+        customer.setPassword("1234");
+        customer.setFirstName("pay");
+        assertEquals(customer.getUsername(), customerDao.registerCustomer(customer).getUsername());
+    }
+}
---
+++ src/test/java/com/finalproject/walletforex/WalletTest.java
@@ -0,0 +1,77 @@
+package com.finalproject.walletforex;
+
+
+import com.finalproject.walletforex.dao.CustomerDao;
+import com.finalproject.walletforex.dao.WalletDao;
+import com.finalproject.walletforex.dto.CustomerDto;
+import com.finalproject.walletforex.dto.WalletDto;
+import com.finalproject.walletforex.exception.UserAlreadyException;
+import com.finalproject.walletforex.model.Customer;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class WalletTest {
+
+    @Autowired
+    private WalletDao walletDao;
+
+    @Autowired
+    private CustomerDao customerDao;
+
+    @Rule
+    public ExpectedException rule = ExpectedException.none();
+
+//    Wallet addWallet(WalletDto dto);
+//    List<Wallet> findByCif(String cif);
+
+    @Before
+    public void setDao() throws UserAlreadyException {
+        CustomerDto customer = new CustomerDto();
+        customer.setUsername("agzwi");
+        customer.setPassword("1234567");
+        customer.setFirstName("agzwibawa");
+        customerDao.registerCustomer(customer);
+
+        Customer customer1 = new Customer();
+        customer1.setCif("ag-01");
+
+        WalletDto walletDto = new WalletDto();
+        walletDto.setCustomer(customer1);
+        walletDto.setWalletName("istri1");
+        walletDao.addWallet(walletDto);
+    }
+
+    @Test
+    public void testAdd() throws UserAlreadyException {
+        CustomerDto customer = new CustomerDto();
+        customer.setUsername("nofita");
+        customer.setPassword("12345");
+        customer.setFirstName("nofi");
+        customerDao.registerCustomer(customer);
+
+        Customer customer1 = new Customer();
+        customer1.setCif("no-11");
+
+        WalletDto walletDto = new WalletDto();
+        walletDto.setCustomer(customer1);
+        walletDto.setWalletName("istri");
+        assertEquals("istri", walletDao.addWallet(walletDto).getWalletName());
+    }
+
+    @Test
+    public void getByCif() {
+        assertEquals("istri1", walletDao.findByCif("ag-01").get(0).getWalletName());
+    }
+
+
+}
